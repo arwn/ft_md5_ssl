6cce6de7c781b1a93151bba13d43a01b486e2066
arwn <windham.aren@protonmail.com>
2019-03-12 13:39:40 -0700

    lol

--- md5.h
+++ md5.h
@@ -6,51 +6,30 @@
 /*   By: awindham <awindham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/08 13:09:23 by zfaria            #+#    #+#             */
-/*   Updated: 2019/03/12 13:34:39 by awindham         ###   ########.fr       */
+/*   Updated: 2019/03/12 13:39:30 by awindham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MD5_H
-#define MD5_H
+# define MD5_H
 
 typedef struct
 {
-	unsigned int count[2];
-	unsigned int state[4];
-	unsigned char buffer[64];   
-} MD5_CTX;
+	unsigned int	count[2];
+	unsigned int	state[4];
+	unsigned char	buffer[64];
+}					t_md5_ctx;
 
+# define F(x,y,z) ((x & y) | (~x & z))
+# define G(x,y,z) ((x & z) | (y & ~z))
+# define H(x,y,z) (x^y^z)
+# define I(x,y,z) (y ^ (x | ~z))
+# define ROTATE_LEFT(x,n) ((x << n) | (x >> (32-n)))
 
-#define F(x,y,z) ((x & y) | (~x & z))
-#define G(x,y,z) ((x & z) | (y & ~z))
-#define H(x,y,z) (x^y^z)
-#define I(x,y,z) (y ^ (x | ~z))
-#define ROTATE_LEFT(x,n) ((x << n) | (x >> (32-n)))
-
-#define FF(a,b,c,d,x,s,ac) \
-{ \
-	a += F(b,c,d) + x + ac; \
-	a = ROTATE_LEFT(a,s); \
-	a += b; \
-}
-#define GG(a,b,c,d,x,s,ac) \
-{ \
-	a += G(b,c,d) + x + ac; \
-	a = ROTATE_LEFT(a,s); \
-	a += b; \
-}
-#define HH(a,b,c,d,x,s,ac) \
-{ \
-	a += H(b,c,d) + x + ac; \
-	a = ROTATE_LEFT(a,s); \
-	a += b; \
-}
-#define II(a,b,c,d,x,s,ac) \
-{ \
-	a += I(b,c,d) + x + ac; \
-	a = ROTATE_LEFT(a,s); \
-	a += b; \
-}
+# define FF(a,b,c,d,x,s,ac){a += F(b,c,d) + x + ac;a = ROTATE_LEFT(a,s);a += b;}
+# define GG(a,b,c,d,x,s,ac){a += G(b,c,d) + x + ac;a = ROTATE_LEFT(a,s);a += b;}
+# define HH(a,b,c,d,x,s,ac){a += H(b,c,d) + x + ac;a = ROTATE_LEFT(a,s);a += b;}
+# define II(a,b,c,d,x,s,ac){a += I(b,c,d) + x + ac;a = ROTATE_LEFT(a,s);a += b;}
 
 # define F0	FF(a, b, c, d, x[0], 7, 0xd76aa478);
 # define F1	F0; FF(d, a, b, c, x[1], 12, 0xe8c7b756);
@@ -118,12 +97,16 @@ typedef struct
 # define I15 I14; II(b, c, d, a, x[9], 21, 0xeb86d391);
 # define FUN I15;
 
-void md5_init(MD5_CTX *context);
-void md5_update(MD5_CTX *context, unsigned char *input, unsigned int inputlen);
-void md5_final(MD5_CTX *context, unsigned char digest[16]);
-void md5_transform(unsigned int state[4], unsigned char block[64]);
-void md5_encode(unsigned char *output, unsigned int *input, unsigned int len);
-void md5_decode(unsigned int *output, unsigned char *input, unsigned int len);
+void				md5_init(t_md5_ctx *context);
+void				md5_update(t_md5_ctx *context,
+unsigned char *input, unsigned int inputlen);
+void				md5_final(t_md5_ctx *context,
+unsigned char digest[16]);
+void				md5_transform(unsigned int state[4],
+unsigned char block[64]);
+void				md5_encode(unsigned char *output,
+unsigned int *input, unsigned int len);
+void				md5_decode(unsigned int *output,
+unsigned char *input, unsigned int len);
 
 #endif
-
